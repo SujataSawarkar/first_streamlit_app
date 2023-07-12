64a3989984ce882109f1aefe3b0fa67a0471406c
SujataSawarkar <139342808+SujataSawarkar@users.noreply.github.com>
2023-07-12 22:16:21 +0530

    Update streamlit_app.py
    using python

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,10 @@
 import streamlit
 streamlit.title("this is my own file")
 streamlit.title("this is my new car and the car is imaginary"
-               "this is the reason why i love you")
-streamlit.header("very good idea")
-
+               "this is the reason why i love you")streamlit.header("very good idea")
 streamlit.text("i can do my work properly")
+
+
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
