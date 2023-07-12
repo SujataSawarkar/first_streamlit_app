20c9d877040e2854efc8c979676dacde51f378c9
SujataSawarkar <139342808+SujataSawarkar@users.noreply.github.com>
2023-07-12 17:24:00 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,4 @@
 import streamlit
 streamlit.title("this is my own file")
-streamlit.title("this is my new car and the car is imaginary")
+streamlit.title("this is my new car and the car is imaginary"
+               "this is the reason why i love you")
