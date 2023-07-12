4a825e95672562fe44d5da6de3c66a083360c9ff
SujataSawarkar <139342808+SujataSawarkar@users.noreply.github.com>
2023-07-12 22:17:02 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
 import streamlit
 streamlit.title("this is my own file")
 streamlit.title("this is my new car and the car is imaginary"
-               "this is the reason why i love you")streamlit.header("very good idea")
+               "this is the reason why i love you")
+streamlit.header("very good idea")
 streamlit.text("i can do my work properly")
 
 
