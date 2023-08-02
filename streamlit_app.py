e82bafcaba3767b3a264ae5acb676026002b0a4c
SujataSawarkar <139342808+SujataSawarkar@users.noreply.github.com>
2023-08-02 13:54:08 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,8 +15,8 @@ import requests
 
 streamlit.header("Fruityvice Fruit Advice!")
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())# just write the data to the screen
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "Kiwi")
+
 
 # write your own comment -what does the next line do? 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
